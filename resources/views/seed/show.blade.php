8cca0a97011abaa6626b9b119108f9fa7d744010
ipiedrahiv <ipiedrahiv@eafit.edu.co>
2021-03-13 10:31:36 -0500

    Seed lista and show

---
+++ resources/views/seed/show.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section("title", $data["title"])
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ $data["seed"]->getName() }}</div>
+                <div class="card-body">
+                    <b>Product name:</b> {{ $data["seed"]->getName() }}<br />
+                    <b>Product seller:</b> {{ $data["seed"]->getSeller() }}<br /><br />
+                    <b>Product price:</b> {{ $data["seed"]->getPrice() }}<br /><br />
+                    <b>Product categories:</b> {{ $data["seed"]->getCategories() }}<br /><br />
+                    <b>Product keywords:</b> {{ $data["seed"]->getKeywords() }}<br /><br />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
