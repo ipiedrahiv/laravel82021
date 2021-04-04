f66f8106b011c9eadc4563328981686b3f53847c
Isabel Piedrahita <46933373+ipiedrahiv@users.noreply.github.com>
2021-04-04 12:31:42 -0500

    Faromatter back
    Placing formatter back in the deployed version.

--- config/app.php
+++ config/app.php
@@ -161,6 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        FORMATTER
+        MattAllan\LaravelCodeStyle\ServiceProvider::class,
 
         /*
          * Package Service Providers...
