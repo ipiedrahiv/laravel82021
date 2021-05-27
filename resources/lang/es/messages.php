0c9ea50b24a12cb40a1e79c8c9c057d641e1eddf
SamtiagoSR <samtiago.santa.ramirez@gmail.com>
2021-05-27 16:38:35 -0500

    see final results

--- resources/lang/es/messages.php
+++ resources/lang/es/messages.php
@@ -3,5 +3,6 @@
 return [
     'success' => 'Añadido exitosamente!',
     'remove' => 'Removido exitosamente !',
+    'message' => 'Este es tu recibo, gracias por apoyar a las empresas locales y creer en nuestra página.',
 
 ];
